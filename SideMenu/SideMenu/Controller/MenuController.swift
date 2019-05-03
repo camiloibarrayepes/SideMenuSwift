81801cc2e519fd21da000f128eb2a2ba184f416a
camilo andres ibarra yepes <cibarra@grupoalto.com>
2019-05-02 22:50:36 -0500

    Added tableview and cells for menu options

--- SideMenu/SideMenu/Controller/MenuController.swift
+++ SideMenu/SideMenu/Controller/MenuController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+private let reuseIdentifer = "MenuOptionCell"
 
 class MenuController: UIViewController{
     //MARK properties
+    var tableView: UITableView!
     //MARK init
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     //MARK Handlres
+    func configureTableView(){
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(MenuOptionCell.self, forCellReuseIdentifier: reuseIdentifer)
+    }
+}
+
+extension MenuController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifer, for: indexPath) as! MenuOptionCell
+        return cell
+    }
+    
+    
 }
---
+++ SideMenu/SideMenu/View/MenuOptionCell.swift
@@ -0,0 +1,27 @@
+//
+//  MenuOptionCell.swift
+//  SideMenu
+//
+//  Created by camilo andres ibarra yepes on 5/2/19.
+//  Copyright © 2019 camilo andres ibarra yepes. All rights reserved.
+//
+
+import UIKit
+
+class MenuOptionCell: UITableViewCell {
+
+    //MARK properties
+    
+    //MARK init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK handlers
+
+}
